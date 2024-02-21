30ee327f22a68fbc229cc412fefb0726ddaafbfd
Massidama <massidama@outlook.it>
2024-02-21 11:22:19 +0100

    create controller, factory, seeder

--- database/migrations/2024_02_21_095313_create_projects_table.php
+++ database/migrations/2024_02_21_095313_create_projects_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('_projects', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
 
             $table->string('nome');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('_projects');
+        Schema::dropIfExists('projects');
     }
 };
